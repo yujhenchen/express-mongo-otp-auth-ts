a15809e5a8a87d9514dacd8426e9666050c08834
yujhenchen <yujhen.chen93@gmail.com>
2024-06-08 15:15:18 +0800

    feat: add mongoose user modal

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,71 +1,109 @@
-import moment from 'moment';
+// import moment from 'moment';
 
 
-// **** Variables **** //
+// // **** Variables **** //
 
-const INVALID_CONSTRUCTOR_PARAM = 'nameOrObj arg must a string or an object ' + 
-  'with the appropriate user keys.';
+// const INVALID_CONSTRUCTOR_PARAM = 'nameOrObj arg must a string or an object ' +
+//   'with the appropriate user keys.';
 
 
-// **** Types **** //
+// // **** Types **** //
 
-export interface IUser {
-  id: number;
+// export interface IUser {
+//   id: number;
+//   name: string;
+//   email: string;
+//   created: Date;
+// }
+
+
+// // **** Functions **** //
+
+// /**
+//  * Create new User.
+//  */
+// function new_(
+//   name?: string,
+//   email?: string,
+//   created?: Date,
+//   id?: number, // id last cause usually set by db
+// ): IUser {
+//   return {
+//     id: (id ?? -1),
+//     name: (name ?? ''),
+//     email: (email ?? ''),
+//     created: (created ? new Date(created) : new Date()),
+//   };
+// }
+
+// /**
+//  * Get user instance from object.
+//  */
+// function from(param: object): IUser {
+//   if (!isUser(param)) {
+//     throw new Error(INVALID_CONSTRUCTOR_PARAM);
+//   }
+//   const p = param as IUser;
+//   return new_(p.name, p.email, p.created, p.id);
+// }
+
+// /**
+//  * See if the param meets criteria to be a user.
+//  */
+// function isUser(arg: unknown): boolean {
+//   return (
+//     !!arg &&
+//     typeof arg === 'object' &&
+//     'id' in arg && typeof arg.id === 'number' &&
+//     'email' in arg && typeof arg.email === 'string' &&
+//     'name' in arg && typeof arg.name === 'string' &&
+//     'created' in arg && moment(arg.created as string | Date).isValid()
+//   );
+// }
+
+
+// // **** Export default **** //
+
+// export default {
+//   new: new_,
+//   from,
+//   isUser,
+// } as const;
+
+import { Schema, model } from 'mongoose';
+
+export type TUser = {
+  // id: number;
   name: string;
   email: string;
-  created: Date;
-}
+  password: string;
+  role: 'admin' | 'visitor';
+};
 
+const userSchema: Schema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: {
+      values: ['admin', 'visitor'],
+      message: '{ROLE} does not exist',
+    },
+  },
+});
 
-// **** Functions **** //
+const User = model<TUser>('User', userSchema);
 
-/**
- * Create new User.
- */
-function new_(
-  name?: string,
-  email?: string,
-  created?: Date,
-  id?: number, // id last cause usually set by db
-): IUser {
-  return {
-    id: (id ?? -1),
-    name: (name ?? ''),
-    email: (email ?? ''),
-    created: (created ? new Date(created) : new Date()),
-  };
-}
-
-/**
- * Get user instance from object.
- */
-function from(param: object): IUser {
-  if (!isUser(param)) {
-    throw new Error(INVALID_CONSTRUCTOR_PARAM);
-  }
-  const p = param as IUser;
-  return new_(p.name, p.email, p.created, p.id);
-}
-
-/**
- * See if the param meets criteria to be a user.
- */
-function isUser(arg: unknown): boolean {
-  return (
-    !!arg &&
-    typeof arg === 'object' &&
-    'id' in arg && typeof arg.id === 'number' && 
-    'email' in arg && typeof arg.email === 'string' && 
-    'name' in arg && typeof arg.name === 'string' &&
-    'created' in arg && moment(arg.created as string | Date).isValid()
-  );
-}
-
-
-// **** Export default **** //
-
-export default {
-  new: new_,
-  from,
-  isUser,
-} as const;
+export default User;
