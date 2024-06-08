adbfffad1a7e6714ad99ce5bc8ebd91ed93a8581
yujhenchen <yujhen.chen93@gmail.com>
2024-06-08 16:04:49 +0800

    fix: eslint tab issue

---
+++ src/.prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  tabs: false,
+  semi: true,
+  quoteProps: "as-needed",
+  bracketSpacing: true,
+  bracketSameLine: false,
+  htmlWhitespaceSensitivity: "strict",
+};
