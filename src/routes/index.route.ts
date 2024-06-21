43fdb566ebbbbf44ae1bddd3fc2741fa62123a42
yujhenchen <yujhen.chen93@gmail.com>
2024-06-21 16:00:08 +0800

    feat: use auth and user route

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
+import authRoutes from "./auth.route";
+import userRoutes from "./user.route";
 
 const router = Router();
 
 router.get('/', (req, res) => {
     res.send('Hello World!')
-})
+});
+
+router.use('/auth', authRoutes);
+router.use('/user', userRoutes);
 
 export default router;
