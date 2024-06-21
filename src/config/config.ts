f0c11fb3b88e859cabfb1c859ef66660d11bd784
yujhenchen <yujhen.chen93@gmail.com>
2024-06-21 11:58:38 +0800

    feat: add jsonwebtoken

--- src/config/config.ts
+++ src/config/config.ts
@@ -17,9 +17,9 @@ const envVarsSchema = Joi.object({
     //     then: Joi.boolean().default(true),
     //     otherwise: Joi.boolean().default(false),
     // }),
-    // JWT_SECRET: Joi.string()
-    //     .required()
-    //     .description('JWT Secret required to sign'),
+    JWT_SECRET: Joi.string().default(process.env.JWT_SECRET)
+        .required()
+        .description('JWT Secret required to sign'),
     // MONGO_HOST: Joi.string().required().description('Mongo DB host url'),
     // MONGO_PORT: Joi.number().default(27017),
     DB_CONN_STRING: Joi.string().default(process.env.DB_CONN_STRING)
@@ -36,7 +36,7 @@ const config = {
     env: envVars.NODE_ENV,
     port: envVars.SERVER_PORT,
     // mongooseDebug: envVars.MONGOOSE_DEBUG,
-    // jwtSecret: envVars.JWT_SECRET,
+    jwtSecret: envVars.JWT_SECRET,
     // frontend: envVars.MEAN_FRONTEND || 'angular',
     // mongo: {
     //     host: envVars.MONGO_HOST,
