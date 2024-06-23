dc6b96bf058083b1c9c8b2f552ec9ed449e52bf2
yujhenchen <yujhen.chen93@gmail.com>
2024-06-23 15:32:57 +0800

    fix: remove not need algorithm, clean up

--- src/utils/authHelper.ts
+++ src/utils/authHelper.ts
@@ -3,11 +3,7 @@ import config from "config/config";
 
 export default function generateJWTToken(payload: object, expiresIn = 86400): string {
     try {
-        // const payload = { name: userName };
-        return jwt.sign(payload, config.jwtSecret, {
-            algorithm: 'HS256',
-            expiresIn
-        });
+        return jwt.sign(payload, config.jwtSecret, { expiresIn });
     } catch (error) {
         console.error(error);
         return '';
