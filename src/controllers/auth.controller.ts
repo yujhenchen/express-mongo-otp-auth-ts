34b29dc277b26a3b3f6a43c979c18a0489c439ea
yujhenchen <yujhen.chen93@gmail.com>
2024-06-23 15:47:17 +0800

    feat: use http-status

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -2,27 +2,27 @@
 import { findOneUser, insertUser } from './user.controller';
 import { Request, Response } from 'express';
 import bcrypt from 'bcrypt';
-
+import status from 'http-status';
 
 export async function signUp(req: Request, res: Response): Promise<void> {
     try {
         const { name } = req.body;
         const user = await findOneUser(name);
         if (user) {
-            res.status(409).send({ message: `User already exists: ${name}` });
+            res.status(status.CONFLICT).send({ message: `User already exists: ${name}` });
             return;
         }
 
         const newUser = await insertUser(req.body);
         if (!newUser) {
-            res.status(500).send({ message: 'User registered Failed' });
+            res.status(status.INTERNAL_SERVER_ERROR).send({ message: 'User registered Failed' });
             return;
         }
 
         req.body = newUser.toJSON();
-        res.status(200).send({ message: "User registered successfully!" });
+        res.status(status.OK).send({ message: "User registered successfully!" });
     } catch (error) {
-        res.status(500).send({ message: error });
+        res.status(status.INTERNAL_SERVER_ERROR).send({ message: error });
     }
 
 }
@@ -32,13 +32,13 @@ export async function signIn(req: Request, res: Response): Promise<void> {
         const { name, password } = req.body;
         const user = await findOneUser(name);
         if (!user) {
-            res.status(404).send({ message: `User Not found. User name: ${name}` });
+            res.status(status.NOT_FOUND).send({ message: `User Not found. User name: ${name}` });
             return;
         }
 
         const isPasswordValid = bcrypt.compareSync(password, user.password);
         if (!isPasswordValid) {
-            res.status(401).send({
+            res.status(status.UNAUTHORIZED).send({
                 message: "Invalid Password!",
             });
             return;
@@ -46,18 +46,18 @@ export async function signIn(req: Request, res: Response): Promise<void> {
 
         const token = await user.generateToken();
         if (!token) {
-            res.status(500).send({ message: 'Failed to generate token' });
+            res.status(status.INTERNAL_SERVER_ERROR).send({ message: 'Failed to generate token' });
             return;
         }
 
-        res.status(200).send({
+        res.status(status.OK).send({
             name: user.name,
             email: user.email,
             // role: user.role,
             token
         });
     } catch (error) {
-        res.status(500).send({ message: error });
+        res.status(status.INTERNAL_SERVER_ERROR).send({ message: error });
     }
 }
 
@@ -67,13 +67,13 @@ export async function signOut(req: Request, res: Response): Promise<void> {
         const { name, password } = req.body;
         const user = await findOneUser(name);
         if (!user) {
-            res.status(404).send({ message: `User Not found. User name: ${name}` });
+            res.status(status.NOT_FOUND).send({ message: `User Not found. User name: ${name}` });
             return;
         }
 
         const isPasswordValid = bcrypt.compareSync(password, user.password);
         if (!isPasswordValid) {
-            res.status(401).send({
+            res.status(status.UNAUTHORIZED).send({
                 message: "Invalid Password!",
             });
             return;
@@ -81,8 +81,8 @@ export async function signOut(req: Request, res: Response): Promise<void> {
 
         await user.deleteToken();
 
-        res.status(200).send({ message: "You've been signed out!" });
+        res.status(status.OK).send({ message: "You've been signed out!" });
     } catch (error) {
-        res.status(500).send({ message: error });
+        res.status(status.INTERNAL_SERVER_ERROR).send({ message: error });
     }
 }
