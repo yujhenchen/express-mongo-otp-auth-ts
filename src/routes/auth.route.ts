138ad23e311dad81c41ecac667453d9d70d903ab
yujhenchen <yujhen.chen93@gmail.com>
2024-06-25 17:12:28 +0800

    feat: move timelog to route index

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,22 +1,12 @@
 import { signIn, signOut, signUp } from "controllers/auth.controller";
-import { Request, Response, Router, NextFunction } from 'express';
+import { Router } from 'express';
 
 const authRoutes = Router();
-
-// middleware that is specific to this router
-const timeLog = (req: Request, res: Response, next: NextFunction) => {
-    // console.log('Time: ', Date.now());
-    next();
-}
-
-authRoutes.use(timeLog);
 
 authRoutes.post('/signup', signUp);
 
 authRoutes.post('/signin', signIn);
 
 authRoutes.post('/signout', signOut);
-
-// authRoutes.get('/userinfo');
 
 export default authRoutes;
--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -1,14 +1,22 @@
-import { Router } from "express";
+import { Request, Response, NextFunction, Router } from "express";
 import authRoutes from "./auth.route";
 import userRoutes from "./user.route";
 
+// middleware that is specific to this router
+const timeLog = (req: Request, res: Response, next: NextFunction) => {
+    // console.log('Time: ', Date.now());
+    next();
+}
+
 const router = Router();
+
+router.use(timeLog);
+
+router.use('/auth', authRoutes);
+router.use('/user', userRoutes);
 
 router.get('/', (req, res) => {
     res.send('Hello World!')
 });
-
-router.use('/auth', authRoutes);
-router.use('/user', userRoutes);
 
 export default router;
