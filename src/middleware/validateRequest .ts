f77803eac6ebbd933c9c16203879d8fa45987497
yujhenchen <yujhen.chen93@gmail.com>
2024-06-29 18:38:38 +0800

    feat: handle validation error

--- src/middleware/validateRequest .ts
+++ src/middleware/validateRequest .ts
@@ -1,6 +1,6 @@
 import status from 'http-status';
 import { Request, Response, NextFunction } from "express";
-import { Schema } from "joi";
+import { Schema, ValidationError } from "joi";
 
 const validateRequest = (schema: Schema) => async (req: Request, res: Response, next: NextFunction) => {
 
@@ -9,7 +9,10 @@ const validateRequest = (schema: Schema) => async (req: Request, res: Response, 
         next();
     } catch (error) {
         console.error(error);
-        res.status(status.BAD_REQUEST).json({ message: error });
+
+        if (error instanceof ValidationError) res.status(status.BAD_REQUEST).json({ message: error.details.map(detail => detail.message) });
+        else if (error instanceof Error) res.status(status.INTERNAL_SERVER_ERROR).json({ message: error.message });
+        else res.status(status.INTERNAL_SERVER_ERROR).json({ message: 'Unknown error occurred' });
     }
 }
 
