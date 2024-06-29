39c1a9fbced45aaf5e17af2ea0fc25bf9db50155
yujhenchen <yujhen.chen93@gmail.com>
2024-06-29 18:40:18 +0800

    refactor: update password regex, wrap email option object

--- src/middleware/validators.ts
+++ src/middleware/validators.ts
@@ -1,23 +1,26 @@
 import { UserRole } from "constants/userRoles";
-import Joi from "joi";
+import Joi, { EmailOptions } from "joi";
+
+const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+={}[\]:;<>,.?/~\\|-]).{8,16}$/;
+const emailOptions: EmailOptions = Object.freeze({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } });
 
 export const signupSchema = Joi.object({
     name: Joi.string().min(3).max(30).required(),
-    email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required(),
+    email: Joi.string().email(emailOptions).required(),
     // mobileNumber: Joi.string().regex(/^[1-9][0-9]{9}$/),
-    password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
+    password: Joi.string().pattern(new RegExp(passwordRegex)).required(),
     repeatPassword: Joi.ref('password')
 });
 
 export const signinSchema = Joi.object({
-    email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required(),
-    password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required()
+    email: Joi.string().email(emailOptions).required(),
+    password: Joi.string().pattern(new RegExp(passwordRegex)).required()
 });
 
 export const updateUserSchema = Joi.object({
     name: Joi.string().min(3).max(30),
-    email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
-    password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
+    email: Joi.string().email(emailOptions),
+    password: Joi.string().pattern(new RegExp(passwordRegex)),
     role: Joi.string().valid(...Object.values(UserRole)),
     token: Joi.string(),
 });
