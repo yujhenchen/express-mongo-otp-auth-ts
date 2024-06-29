5da16b3140ce1cbf5dfde041652d85d3cac16cdc
yujhenchen <yujhen.chen93@gmail.com>
2024-06-29 15:38:11 +0800

    fix: get all user route

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -3,11 +3,16 @@ import { Router } from "express";
 
 const userRoutes = Router();
 
+/**
+ * To prevent the route '/all' from being interpreted as a userId parameter,
+ *  causing Mongoose to attempt to cast the string "all" to an ObjectId:
+ *  keep the oder of the routes '/all' before '/:userId'
+ */
+userRoutes.get('/all', getAllUsers);
+
 userRoutes.get('/:userId', getUser);
 
 userRoutes.patch('/:userId', updateUser);
-
-userRoutes.get('/all', getAllUsers);
 
 userRoutes.patch('/change-role/:userId', changeRole);
 
