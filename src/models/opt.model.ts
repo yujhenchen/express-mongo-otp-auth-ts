d74652527be43963ccc8a34e57efb3f76df9b89b
yujhenchen <yujhen.chen93@gmail.com>
2024-07-11 22:48:17 +0800

    feat: define otp schema properties

--- src/models/opt.model.ts
+++ src/models/opt.model.ts
@@ -1,6 +1,27 @@
 import { Schema, model } from "mongoose";
 
-const otpSchema = new Schema({});
+const otpSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        match: [
+            // eslint-disable-next-line no-useless-escape
+            /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            'Please enter a valid email',
+        ],
+    },
+    otp: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        expires: 60 * 5, // The document will be automatically deleted after 5 minutes of its creation time
+    },
+});
 
 const OTP = model('otps', otpSchema, 'otps');
 
