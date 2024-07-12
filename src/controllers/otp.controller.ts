9b7833169b2b05aa9e73b42695127eab34b7dc5f
yujhenchen <yujhen.chen93@gmail.com>
2024-07-12 22:31:47 +0800

    feat: generate otp code until there is no duplicated

--- src/controllers/otp.controller.ts
+++ src/controllers/otp.controller.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from 'express';
 import status from 'http-status';
+import otpGenerator from 'otp-generator';
 import handleErrorResponse from '@/utils/controller.helper';
 import { IUser } from '@/interfaces/user';
 import User from '@/models/user.model';
+import OTP from '@/models/opt.model';
 
 export async function sendOTP(
     req: Request<Record<string, never>, Record<string, never>, Pick<IUser, 'email'>, Record<string, never>>,
@@ -15,6 +17,18 @@ export async function sendOTP(
             res.status(status.NOT_FOUND).json({ message: `User Not found. User email: ${email}` });
             return;
         }
+
+        let otp = otpGenerator.generate(6, { upperCaseAlphabets: false, specialChars: false });
+        /**
+         * find the otp in database,
+         * if the old one exists, generate a new one until it does not exists
+         * */
+        let foundOTP = OTP.findOne({ otp });
+        while (foundOTP) {
+            otp = otpGenerator.generate(6, { upperCaseAlphabets: false, specialChars: false });
+            foundOTP = OTP.findOne({ otp });
+        }
+
 
         res.status(status.OK).json({ message: 'OTP sent' });
     } catch (error) {
