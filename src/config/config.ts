e5d67f9bcaf99408290f1b0a1961c6747d03a048
yujhenchen <yujhen.chen93@gmail.com>
2024-07-14 22:32:17 +0800

    feat: add smtp configs

--- src/config/config.ts
+++ src/config/config.ts
@@ -21,7 +21,12 @@ const envVarsSchema = Joi.object({
         .required()
         .description('JWT Secret required to sign'),
     DB_CONN_STRING: Joi.string().default(process.env.DB_CONN_STRING),
-    DB_NAME: Joi.string().default(process.env.DB_NAME)
+    DB_NAME: Joi.string().default(process.env.DB_NAME),
+    SMTP_HOST: Joi.string().default(process.env.SMTP_HOST),
+    SMTP_PORT: Joi.number().default(process.env.SMTP_PORT),
+    SMTP_USERNAME: Joi.string().default(process.env.SMTP_USERNAME),
+    SMTP_PASSWORD: Joi.string().default(process.env.SMTP_PASSWORD),
+    SMTP_SENDER: Joi.string().default(process.env.SMTP_SENDER)
 })
     .unknown()
     .required();
@@ -38,6 +43,11 @@ const config = {
     jwtSecret: envVars.JWT_SECRET,
     dbConnString: envVars.DB_CONN_STRING,
     dbName: envVars.DB_NAME,
+    smtpHost: envVars.SMTP_HOST,
+    smtpPort: envVars.SMTP_PORT,
+    smtpUsername: envVars.SMTP_USERNAME,
+    smtpPassword: envVars.SMTP_PASSWORD,
+    smtpSender: envVars.SMTP_SENDER
 };
 
 export default config;
